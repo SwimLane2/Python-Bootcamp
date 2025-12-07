ea1310594568967171797ca4748bbb4d3dfcca25
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2025-12-07 07:43:05 +0000

    Day 1 Complete

--- Day 1/Task.py
+++ Day 1/Task.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+#print("Hello World! \nThis is my first Python code.")
+#print(f"Hello Angela")
+
+# Below code asks for the user's name and then prints a greeting message.
+# Write your code below this line 👇
+# print("Hello " + input("What is your name? "    ) + "!") 
+name = input("What is your name? ")
+length = len(name)
+print(f"Name '{name}' has {length} characters.")
+# print("Hello " + name + "!")
+
