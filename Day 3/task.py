09cc7e2d09aea58924b86a0ca7f0d95d7ba576e1
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2025-12-10 06:47:30 +0000

    Day 3 exercises

---
+++ Day 3/task.py
@@ -0,0 +1,83 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# if number % 2 == 0:
+#     print("This is an even number.")
+#     if age <= 12:
+#         print("Please pay £5.")
+#     elif age <= 18:
+#         print("Please pay £7.")
+#     else:
+#         print("Please pay £12.")
+
+# else:
+#     print("This is an odd number.")
+
+# print(10 % 3)
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# age = int(input("What is your age? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     if age <= 12:
+#         print("Please pay £5.")
+#     elif age <= 18:
+#         print("Please pay £7.")
+#     elif 45 <= age <= 55:
+#         print("You can ride for free!")
+#     else:
+#         print("Please pay £12.")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+
+# print("welcome to python pizza delivery!")
+# size = input("What size pizza do you want? S, M, or L ")
+# add_pepperoni = input("Do you want pepperoni? Y or N ")
+# extra_cheese = input("Do you want extra cheese? Y or N ")
+
+# small_pizza = 15
+# medium_pizza = 20
+# large_pizza = 25
+# small_pizza_pepperoni = 2
+# medium_large_pizza_pepperoni = 3
+# extra_cheese_price = 1
+# bill = 0
+
+# if size == "S":
+#     bill += small_pizza
+# elif size == "M":
+#     bill += medium_pizza
+# elif size == "L":
+#     bill += large_pizza
+# else:
+#     print("Invalid size selected.")
+
+# if add_pepperoni == "Y":
+#         bill += small_pizza_pepperoni
+# elif size == "M" or size == "L":
+#      bill += medium_large_pizza_pepperoni
+
+# if extra_cheese == "Y":
+#     bill += extra_cheese_price
+
+
+# print(f"Your final bill is £{bill}.")
+
+a = 5
+b = 7
+ 
+if a >= b and a != b:
+    print("A")
+elif not a >= b and a != b:
+    print("B")
+else:
+    print("C")
+
+
