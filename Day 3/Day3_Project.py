4c8c6a4b0f622f7136e892722ac7ec3bf2374aab
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2025-12-11 05:44:18 +0000

    modified the files

--- Day 3/Day3_Project.py
+++ Day 3/Day3_Project.py
@@ -12,11 +12,14 @@ Your mission is to find the treasure.
             
       ''')
 
-first_choice = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"\n').lower()
+first_choice = input('You\'re at a cross road.' 
+                     'Where do you want to go? Type "left" or "right"\n').lower()
 if first_choice == "left":
-    second_choice = input("You\'ve come to a lake. There is an island in the middle of the lake.\n Type 'wait' to wait for a boat. Type 'swim' to swim across.\n").lower()
+    second_choice = input("You\'ve come to a lake. There is an island in the middle of the lake.\n"
+                    "Type 'wait' to wait for a boat. Type 'swim' to swim across.\n").lower()
     if second_choice == "wait":
-        third_choice = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue.\n Which colour do you choose?\n").lower()
+        third_choice = input("You arrive at the island unharmed. There is a house with 3 doors." 
+                             "One red, one yellow and one blue.\n Which colour do you choose?\n").lower()
         if third_choice == "yellow":
             print("You found the treasure! You Win!")
         elif third_choice == "red":
