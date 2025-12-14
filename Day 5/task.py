2831555baff467e76b381e87a11fb1a2c8e1289a
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2025-12-14 08:39:52 +0000

    Day 5 Complete

--- Day 5/task.py
+++ Day 5/task.py
@@ -1,35 +1,26 @@
-# Rock Paper Scissors ASCII Art
+# fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " yummy")
+#     pass
 
-# Rock
-rock = """
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-"""
+# print(fruit)
 
-# Paper
-paper = """
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-"""
+# below is a 25 list of students scores
+students_scores = [85, 92, 78, 90, 88, 76, 95, 89, 84, 91,
+                   87, 93, 82, 80, 199, 77, 81, 79, 96, 83,
+                   75, 98, 99, 100, 74, 75, 98, 99, 190, 74]
 
-# Scissors
-scissor = """
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-"""
-import random
-rock_paper_scissors = [rock, paper, scissor]
-random_thing = random.randint(0, len(rock_paper_scissors) - 1)
-print(rock_paper_scissors[random_thing])
+# max_score = 0
+# for score in students_scores:
+#     if score > max_score:
+#         max_score = score
+# print(f"The highest score in the class is: {max_score}")
+
+# print(min(students_scores))
+
+sum_of_number = 0
+for number in range(1, 101):
+    sum_of_number += number
+
+print(sum_of_number)
