f124a2081f73ffddfb72f7846ef5258f5ce8dcf9
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2025-12-30 05:49:21 +0000

    Learning Numpy

--- Day 8/task.py
+++ Day 8/task.py
@@ -1,6 +1,20 @@
-def factorial(n):
-    if n == 1:
-        return 1
-    return n * factorial(n - 1)
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     return n * factorial(n - 1)
 
-print(factorial(4))
+# print(factorial(4))
+
+import time
+import numpy as np
+x = np.random.random(100000000)
+
+# Case 1
+start = time.time()
+sum(x) / len(x)
+print(time.time() - start)
+
+# Case 2
+start = time.time()
+np.mean(x)
+print(time.time() - start)
